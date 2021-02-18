40827d0588283da9d81178942505cc636d1b4a86
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-18 15:25:14 +0300

    challange #4 updated

--- script.js
+++ script.js
@@ -313,7 +313,7 @@ GOOD LUCK 😀
 
 const bill = 1000;
 const tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2;
-console.log(`The bill was ${bill}, the tip was ${tip}`);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
 
 
