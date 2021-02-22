04f94ddd4b5cf3a90a045b7e9a6cd6bf60537e26
Mohammed <mohammedsami888@gmail.com>
2021-02-23 00:06:52 +0300

    Arrow Function

--- script.js
+++ script.js
@@ -23,16 +23,42 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
-// Function decleration
-function calcMyAge1(thisYear){
-    return thisYear - 1993;
-}
-const age1 = calcMyAge1(2021);
+// // Function decleration
+// function calcMyAge1(thisYear){
+//     return thisYear - 1993;
+// }
+// const age1 = calcMyAge1(2021);
 
-// Function expression
-const calcMyAge2 = function (thisYear){
-    return thisYear - 1993
-}
-const age2 = calcMyAge2(2021);
+// // Function expression
+// const calcMyAge2 = function (thisYear){
+//     return thisYear - 1993
+// }
+// const age2 = calcMyAge2(2021);
+// console.log(age1, age2);
 
-console.log(age1, age2);
+// Arrow Function
+const calcMyAge3 = thisYear => thisYear - 1993;
+const age3 = calcMyAge3(2021);
+
+console.log(age3);
+
+// Example #1
+const yearsUntilRetirement = birthYear => {
+    const age = 2021 - birthYear
+    const retirement = 60 - age
+    return retirement;
+}
+console.log(yearsUntilRetirement(1993));
+
+// Example #2
+const yearsUntilRetirements = (birthYear, firstName) => {
+    const age = 2021 - birthYear
+    const retirement = 60 - age
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirements(1993, 'Mohammed'));
+console.log(yearsUntilRetirements(1988, 'Amer'));
+
+
+
