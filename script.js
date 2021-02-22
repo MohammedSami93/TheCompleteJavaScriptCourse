dd4d061e0566b2aee574328a5f49e3e9bc045b7d
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-22 18:16:44 +0300

    function decleration vs. expression

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
  'use strict';
 
-function logger() {
-    console.log('My name is Mohammed');
+// function logger() {
+//     console.log('My name is Mohammed');
+// }
+
+// // calling, running, invoking function
+// logger();
+// logger();
+// logger();
+
+
+// function fruitProcessor(apples, oranges){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Function decleration
+function calcMyAge1(thisYear){
+    return thisYear - 1993;
 }
+const age1 = calcMyAge1(2021);
 
-// calling, running, invoking function
-logger();
-logger();
-logger();
-
-
-function fruitProcessor(apples, oranges){
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+// Function expression
+const calcMyAge2 = function (thisYear){
+    return thisYear - 1993
 }
+const age2 = calcMyAge2(2021);
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(age1, age2);
