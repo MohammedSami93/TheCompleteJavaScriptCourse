d008455ab1f69970629c06ecf1d952376d6a99a3
Mohammed <mohammedsami888@gmail.com>
2021-02-23 23:59:13 +0300

    Coding Challenge #2

--- script.js
+++ script.js
@@ -206,36 +206,88 @@ checkWinner(60, 60, 60, 50, 50, 50);
 /* Arrays methods */
 
 
-const friends = ['Amer', 'Mujtaba', 'Ali'];
+// const friends = ['Amer', 'Mujtaba', 'Ali'];
 
-// to add element to the first of the array
-friends.unshift('Abdullah');
-console.log(friends);
+// // to add element to the first of the array
+// friends.unshift('Abdullah');
+// console.log(friends);
 
-// By the defult push function and unshift return the arrays length
-console.log(friends.push('Muneer'));
+// // By the defult push function and unshift return the arrays length
+// console.log(friends.push('Muneer'));
 
-// remove elements
-friends.pop(); // remove lase element
-friends.pop();
-console.log(friends);
+// // remove elements
+// friends.pop(); // remove lase element
+// friends.pop();
+// console.log(friends);
 
-// pop function return the removed element ...
-console.log(friends.pop());
+// // pop function return the removed element ...
+// console.log(friends.pop());
 
-// shift() is to remove the first element
-friends.shift();
-console.log(friends);
+// // shift() is to remove the first element
+// friends.shift();
+// console.log(friends);
 
-// indexOf() is to return the position of element in the array
-console.log(friends.indexOf('Amer'));
-console.log(friends.indexOf('bgfdhngng')); // we'll get -1 if the element is NOT in the array
+// // indexOf() is to return the position of element in the array
+// console.log(friends.indexOf('Amer'));
+// console.log(friends.indexOf('bgfdhngng')); // we'll get -1 if the element is NOT in the array
 
-// to check if the element in the array or not, that mean the result will be True or False
-console.log(friends.includes('Amer'));
-console.log(friends.includes('bgfdhngng'));
+// // to check if the element in the array or not, that mean the result will be True or False
+// console.log(friends.includes('Amer'));
+// console.log(friends.includes('bgfdhngng'));
 
-// example
-if (friends.includes('Amer')){
-    console.log('you have friends called Amer');
+// // example
+// if (friends.includes('Amer')){
+//     console.log('you have friends called Amer');
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Coding Challenge #2 */
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+Test data: 125, 555 and 44
+
+GOOD LUCK 😀
+*/
+
+const calcTip = function(bill) {
+    if(bill >= 50 && bill <= 300){
+        const tip = bill * 0.15;
+        //console.log(tip);
+        return tip;
+    } else {
+        const tip = bill * 0.2;
+        //console.log(tip);
+        return tip;
+    }
 }
+// calcTip(100)
+
+const bills = [125, 555, 44];
+console.log(`bill1: ${bills[0]}
+bill2: ${bills[1]}
+bill3: ${bills[2]}`);
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(`tip1: ${tips[0]}
+tip2: ${tips[1]}
+tip3: ${tips[2]}`);
+
+const total = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
+console.log(`total1: ${total[0]}
+total2: ${total[1]}
+total3: ${total[2]}`);
