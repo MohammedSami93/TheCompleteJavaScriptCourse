8686050c1177de0775c99a88edc1b63c071e1563
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-23 10:08:26 +0300

    function calling other function

--- script.js
+++ script.js
@@ -36,29 +36,44 @@
 // const age2 = calcMyAge2(2021);
 // console.log(age1, age2);
 
-// Arrow Function
-const calcMyAge3 = thisYear => thisYear - 1993;
-const age3 = calcMyAge3(2021);
+// // Arrow Function
+// const calcMyAge3 = thisYear => thisYear - 1993;
+// const age3 = calcMyAge3(2021);
 
-console.log(age3);
+// console.log(age3);
 
-// Example #1
-const yearsUntilRetirement = birthYear => {
-    const age = 2021 - birthYear
-    const retirement = 60 - age
-    return retirement;
+// // Example #1
+// const yearsUntilRetirement = birthYear => {
+//     const age = 2021 - birthYear
+//     const retirement = 60 - age
+//     return retirement;
+// }
+// console.log(yearsUntilRetirement(1993));
+
+// // Example #2
+// const yearsUntilRetirements = (birthYear, firstName) => {
+//     const age = 2021 - birthYear
+//     const retirement = 60 - age
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirements(1993, 'Mohammed'));
+// console.log(yearsUntilRetirements(1988, 'Amer'));
+
+
+/////////////////////////////////////////////////////////////////////////
+
+// function calling other function
+
+function cutFruitPices(fruit){
+    return fruit * 4;
 }
-console.log(yearsUntilRetirement(1993));
 
-// Example #2
-const yearsUntilRetirements = (birthYear, firstName) => {
-    const age = 2021 - birthYear
-    const retirement = 60 - age
-    return `${firstName} retires in ${retirement} years`;
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPices(apples);
+    const orangePieces = cutFruitPices(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
 }
-
-console.log(yearsUntilRetirements(1993, 'Mohammed'));
-console.log(yearsUntilRetirements(1988, 'Amer'));
-
-
-
+console.log(fruitProcessor(2,3));
