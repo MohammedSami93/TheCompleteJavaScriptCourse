9578db672c1cbc41077b3eed8eb791cdceed9613
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-23 16:37:13 +0300

    code challange #1

--- script.js
+++ script.js
@@ -65,15 +65,86 @@
 
 // function calling other function
 
-function cutFruitPices(fruit){
-    return fruit * 4;
+// function cutFruitPices(fruit){
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges){
+//     const applePieces = cutFruitPices(apples);
+//     const orangePieces = cutFruitPices(oranges);
+
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2,3));
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/* Coding Challenge #1 */
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+GOOD LUCK 😀
+*/
+
+// My solution //
+/*
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+function checkWinner(dolphinScore1, dolphinScore2, dolphinScore3, KoalaScore1, KoalaScore2, KoalaScore3){
+    const avgDolhins = calcAverage(dolphinScore1, dolphinScore2, dolphinScore3);
+    const avgKoalas = calcAverage(KoalaScore1, KoalaScore2, KoalaScore3);
+    if (avgDolhins >= avgKoalas*2){
+        console.log(`Dolhins win ${avgDolhins} vs. ${avgKoalas}`);
+    } else if (avgKoalas >= avgDolhins*2) {
+        console.log(`Koalas win ${avgKoalas} vs. ${avgDolhins}`);
+    } else {
+        console.log(`NO WINNER!`);
+    }
 }
 
-function fruitProcessor(apples, oranges){
-    const applePieces = cutFruitPices(apples);
-    const orangePieces = cutFruitPices(oranges);
+checkWinner(60, 60, 60, 50, 50, 50);
+*/
 
-    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-    return juice;
+
+// Instructure Solytion //
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+//console.log(calcAverage(3,4,5));
+
+const scoreDolphin = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+//console.log(scoreDolphin, scoreKoalas);
+
+function checkWinner(avgDolphins, avgKoalas){
+    if (avgDolphins >= avgKoalas*2){
+        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolphins*2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+    } else {
+        console.log(`NO WINNER...`);
+    }
 }
-console.log(fruitProcessor(2,3));
+checkWinner(scoreDolphin, scoreKoalas);
