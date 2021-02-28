12ad4c26377096c7497a13b0bae8594ccbc32f89
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-28 11:30:48 +0300

    n/a

--- script.js
+++ script.js
@@ -448,28 +448,33 @@ tall.
 GOOD LUCK 😀
 */
 
-const mark = {
-    fullName: 'Mark',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function() {
-        return this.mass / (this.height ** 2);
-    }
-}
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         return this.mass / (this.height ** 2);
+//     }
+// }
 
-const john = {
-    fullName: 'John',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function() {
-        return this.mass / (this.height ** 2);
-    }
-}
+// const john = {
+//     fullName: 'John Smith ',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         return this.mass / (this.height ** 2);
+//     }
+// }
 
-if (mark.calcBMI() > john.calcBMI()){
-    console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`);
-} else if (john.calcBMI() > mark.calcBMI()) {
-    console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`);
-} else {
-    console.log(`No Winner...`);
-}
+// if (mark.calcBMI() > john.calcBMI()){
+//     console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`);
+// } else if (john.calcBMI() > mark.calcBMI()) {
+//     console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`);
+// } else {
+//     console.log(`No Winner...`);
+// }
+
+
+////////////////////////////////////////////////////////////////////////
+
+
