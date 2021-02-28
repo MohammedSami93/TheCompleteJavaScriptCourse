e6333da9763eb0b2fa3f83fb64ac025f88d67730
Mohammed Ashour <MAshour@etlco.com.sa>
2021-02-28 17:41:22 +0300

    code challenge #4

--- script.js
+++ script.js
@@ -449,11 +449,7 @@ GOOD LUCK 😀
 */
 
 // const mark = {
-<<<<<<< HEAD
-//     fullName: 'Mark Miller',
-=======
 //     fullName: 'Mark',
->>>>>>> 71b7510e8f6614ea0c2751e5d5f103adfac70d7d
 //     mass: 78,
 //     height: 1.69,
 //     calcBMI: function() {
@@ -462,11 +458,7 @@ GOOD LUCK 😀
 // }
 
 // const john = {
-<<<<<<< HEAD
-//     fullName: 'John Smith ',
-=======
 //     fullName: 'John',
->>>>>>> 71b7510e8f6614ea0c2751e5d5f103adfac70d7d
 //     mass: 92,
 //     height: 1.95,
 //     calcBMI: function() {
@@ -483,10 +475,6 @@ GOOD LUCK 😀
 // }
 
 
-<<<<<<< HEAD
-////////////////////////////////////////////////////////////////////////
-
-=======
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* for loop */
@@ -555,22 +543,120 @@ GOOD LUCK 😀
 
 /////////////////////////////////////////////////////////////////////
 
-for(let i = 0; i <= 10; i++){
-    // console.log(`for loop: player ${i}`);
+// for(let i = 0; i <= 10; i++){
+//     // console.log(`for loop: player ${i}`);
+// }
+
+// let i = 0
+// while(i <= 10){
+//     // console.log(`while loop: player ${i}`);
+//     i++;
+// }
+
+// // this line is to export random dicemal number between 1 & 10
+// let dice = Math.trunc(Math.random() * 10 + 1);
+
+// while (dice !== 6){
+//     console.log(`the number is: ${dice}`);
+//     dice = Math.trunc(Math.random() * 10 + 1);
+//     if (dice === 6) console.log(`you got ${dice}, so it's the end of the game!`)
+// }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+/* Coding Challenge #4 */
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+
+
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+//// My Solution
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// for(let i=0; i < bills.length; i++){
+    
+//     const calcTip = function(bills) {
+//         if(bills[i] >= 50 && bills[i] <= 300){
+//             tips.push(bills[i] * 0.15);
+//             totals.push(bills[i] + tips[i]);
+//             return tips, totals;
+//         } else {
+//             tips.push(bills[i] * 0.2);
+//             totals.push(bills[i] + tips[i]);
+//             return tips, totals;
+//         }
+//     }
+//     calcTip(bills);
+// }
+
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
+
+
+// instructor solution
+const calcTip = function (bill){
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-let i = 0
-while(i <= 10){
-    // console.log(`while loop: player ${i}`);
-    i++;
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
 }
 
-// this line is to export random dicemal number between 1 & 10
-let dice = Math.trunc(Math.random() * 10 + 1);
+console.log(bills);
+console.log(tips);
+console.log(totals);
 
-while (dice !== 6){
-    console.log(`the number is: ${dice}`);
-    dice = Math.trunc(Math.random() * 10 + 1);
-    if (dice === 6) console.log(`you got ${dice}, so it's the end of the game!`)
+
+// Bonus question:
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    // console.log(sum)
+    return sum / arr.length;
 }
->>>>>>> 71b7510e8f6614ea0c2751e5d5f103adfac70d7d
+// let avrg = sum / arr.length;
+
+// calcAverage(bills);
+console.log(`bills average: ${calcAverage(bills)}`);
+console.log(`totals average: ${calcAverage(totals)}`);
+console.log(`tips average: ${calcAverage(tips)}`);
+
