e1669731e5b293765ed99676c05d65f24a9eb74a
Mohammed Ashour <MAshour@etlco.com.sa>
2021-03-02 16:02:55 +0300

    min & max

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// finding the maximun and the minimum value of the array
+let temps = [10, 15, 1, "error", 5, 50, 30];
+
+const calcTemp = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+
+  console.log(`maximum value: ${max}`);
+  console.log(`minimum value: ${min}`);
+};
+
+calcTemp(temps);
