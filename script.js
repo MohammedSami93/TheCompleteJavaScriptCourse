05779f92bb468b65a9bf813c453571d5e0b6071b
Mohammed Ashour <MAshour@etlco.com.sa>
2021-03-03 11:55:13 +0300

    querySelector

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Answer 🎉';
+
+console.log(console.log(document.querySelector('.message').textContent));
+
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
